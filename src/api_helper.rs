a186ae1bd97b18cb69795966673cecf76b4cd113
TheAwiteb <awiteb@hotmail.com>
2023-02-22 17:36:46 +0300

    Improve the code structure

--- src/api_helper.rs
+++ src/api_helper.rs
@@ -1,14 +1,20 @@
-use std::future::IntoFuture;
+use std::{
+    future::{Future, IntoFuture},
+    pin::Pin,
+};
 
-use crate::errors::{Error, ErrorMessage};
+use crate::errors::{Error, ErrorMessage, Result as OxideResult};
 use reqwest::RequestBuilder;
 use serde_json::json;
 use uuid::Uuid;
 
 /// Add the token to the request if the token is not `None`.
-pub async fn add_token(request: RequestBuilder, client_token: Option<String>) -> RequestBuilder {
+pub async fn add_token(
+    request: RequestBuilder,
+    client_token: Option<impl AsRef<str>>,
+) -> RequestBuilder {
     if let Some(token) = client_token {
-        request.header("Authorization", format!("Bearer {token}"))
+        request.header("Authorization", format!("Bearer {}", token.as_ref()))
     } else {
         request
     }
@@ -57,24 +63,33 @@ impl<'a> Endpoints<'a> {
     /// Returns the uri of the endpoint.
     pub fn uri(&self) -> String {
         match self {
-            Endpoints::Register { base_url, .. } => format!("{base_url}/api/auth/register"),
-            Endpoints::Login { base_url, .. } => format!("{base_url}/api/auth/login"),
-            Endpoints::GetTodo { base_url, uuid, .. } => format!("{base_url}/api/todo/{uuid}"),
+            Self::Register { base_url, .. } => format!("{base_url}/api/auth/register"),
+            Self::Login { base_url, .. } => format!("{base_url}/api/auth/login"),
+            Self::GetTodo { base_url, uuid, .. } => format!("{base_url}/api/todo/{uuid}"),
         }
     }
     /// Returns the method of the endpoint.
     pub fn method(&self) -> reqwest::Method {
+        use Endpoints::*;
         match self {
-            Endpoints::Register { .. } => reqwest::Method::POST,
-            Endpoints::Login { .. } => reqwest::Method::POST,
-            Endpoints::GetTodo { .. } => reqwest::Method::GET,
+            Register { .. } | Login { .. } => reqwest::Method::POST,
+            GetTodo { .. } => reqwest::Method::GET,
+        }
+    }
+    /// Returns the user token if the endpoint requires the user to be logged in.
+    /// This will return `None` if the endpoint does not require the user to be logged in.
+    pub fn token(&self) -> Option<&str> {
+        use Endpoints::*;
+        match self {
+            Register { .. } | Login { .. } => None,
+            GetTodo { token, .. } => Some(token),
         }
     }
 }
 
 impl<'a> IntoFuture for Endpoints<'a> {
-    type Output = Result<serde_json::Value, Error>;
-    type IntoFuture = std::pin::Pin<Box<dyn std::future::Future<Output = Self::Output> + 'a>>;
+    type Output = OxideResult<serde_json::Value>;
+    type IntoFuture = Pin<Box<dyn Future<Output = Self::Output> + 'a>>;
 
     fn into_future(self) -> Self::IntoFuture {
         Box::pin(async move {
@@ -96,17 +111,11 @@ impl<'a> IntoFuture for Endpoints<'a> {
             };
             // All the endpoints require the user to be logged in except the register and login endpoints.
             response_result(
-                add_token(
-                    req,
-                    match self {
-                        Endpoints::Register { .. } | Endpoints::Login { .. } => None,
-                        Endpoints::GetTodo { token, .. } => Some(token.to_string()),
-                    },
-                )
-                .await
-                .send()
-                .await
-                .map_err(Error::ReqwestError)?,
+                add_token(req, self.token())
+                    .await
+                    .send()
+                    .await
+                    .map_err(Error::ReqwestError)?,
             )
             .await
         })
