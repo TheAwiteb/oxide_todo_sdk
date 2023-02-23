6f320f635c9903949dcad1902be31b028a3cab49
TheAwiteb <awiteb@hotmail.com>
2023-02-23 18:42:48 +0300

    Get a todo by uuid in todo exmaple

--- examples/todo.rs
+++ examples/todo.rs
@@ -4,12 +4,22 @@ use oxide_todo_sdk::Client;
 
 #[tokio::main]
 async fn main() -> OxideTodoResult<()> {
-    let todo = Client::new("http://localhost:8080")
+    // The user we will use.
+    let user = Client::new("http://localhost:8080")
         .login("username", "password")
-        .await?
-        .create_todo("Some new todo")
+        .await?;
+    // Get todo by uuid.
+    let todo = user
+        .todo_by_uuid("a26a61cc-8c2e-4237-977d-4ce0195735c3".parse().unwrap())
+        .await?;
+    println!("Todo: {todo:#?}");
+
+    // Create a new todo.
+    let todo = user
+        .create_todo("Some new todo24")
         .set_status(TodoStatus::Completed)
         .await?;
-    println!("Todo created: {todo:?}");
+    println!("Todo created: {todo:#?}");
+
     Ok(())
 }
